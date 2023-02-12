ae13ac7e8af12b067e24d9deb9ac902623fc0843
nati-d <74787412+nati-d@users.noreply.github.com>
2023-02-11 23:16:34 -0800

    Create daily-temperature.cpp

---
+++ daily-temperature.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> st;
+        int n=temperatures.size();
+        vector<int> ans(n);
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty() && temperatures[st.top()]<temperatures[i])
+            {
+                ans[st.top()]=i-st.top();
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
