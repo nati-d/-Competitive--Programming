fbe5026da312925bc472eec9edcf5952b7265068
nati-d <74787412+nati-d@users.noreply.github.com>
2023-02-12 00:38:21 -0800

    Create rotate-array.cpp

---
+++ rotate-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        while(k>nums.size())
+        {
+            k=k-nums.size() ;
+        }
+        
+        reverse(nums.begin() ,nums.end()) ;
+        reverse(nums.begin() ,nums.begin()+k) ;
+        reverse(nums.begin()+k ,nums.end()) ;
+    }
+};
