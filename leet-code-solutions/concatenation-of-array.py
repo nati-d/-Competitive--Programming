60b1cfc7f776994ac35c1802d5a334e1dce4a959
Nathnael Desalegn <74787412+nati-d@users.noreply.github.com>
2023-12-04 18:37:43 +0300

    Added solution for Concatenation of Array

---
+++ leet-code-solutions/concatenation-of-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans =[]
+        for i in range(len(nums)):
+            ans.append(nums[i])
+        for i in range(len(nums)):
+            ans.append(nums[i])
+        return ans
