a555ff2f06681b970e8fe70e159b597db0818060
Nathnael Desalegn <74787412+nati-d@users.noreply.github.com>
2023-12-13 07:17:59 +0300

    Added solution for Design an Ordered Stream

--- leet-code-solutions/design-an-ordered-stream.py
+++ leet-code-solutions/design-an-ordered-stream.py
@@ -1,17 +1,17 @@
 class OrderedStream:
 
     def __init__(self, n: int):
-        self.stream = [None]* n
+        self.values = [None]* n
         self.ptr = 0
         
 
     def insert(self, idKey: int, value: str) -> List[str]:
-        self.stream[idKey -1] = value
+        self.values[idKey -1] = value
 
         result = []
 
-        while self.ptr< len(self.stream) and self.stream[self.ptr] is not None:
-            result.append(self.stream[self.ptr])
+        while self.ptr< len(self.values) and self.values[self.ptr] is not None:
+            result.append(self.values[self.ptr])
             self.ptr+=1
         return result
         
