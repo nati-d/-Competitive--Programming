9ac34953e59dba13d75a7bc76f422b3cfd915c44
Nathnael Desalegn <74787412+nati-d@users.noreply.github.com>
2023-12-21 19:03:05 +0300

    Added solution for Increasing Triplet Subsequence

---
+++ leet-code-solutions/increasing-triplet-subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        s1 = inf
+        s2 = inf
+
+        for i in nums:
+            if i > s2:
+                return True
+            elif i <= s1:
+                s1 = i
+            else:
+                s2 = i
+        return False
+        
+
+        
