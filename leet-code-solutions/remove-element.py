74d0cc7f4ca4d96796553773aef513a84aeebf19
Nathnael Desalegn <74787412+nati-d@users.noreply.github.com>
2024-01-08 21:49:08 +0300

    Added solution for Remove Element

---
+++ leet-code-solutions/remove-element.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        
+        for i in nums[:]:
+            if i == val:
+                nums.remove(val)
+        return len(nums)
