f159e48d67592b9fd2071b9031c1040f675c7db5
Nathnael Desalegn <74787412+nati-d@users.noreply.github.com>
2024-05-31 11:40:22 +0300

    Update frog1.py

--- atcoder/frog1.py
+++ atcoder/frog1.py
@@ -1,1 +1,9 @@
 
+n = int(input())
+lst = list(map(int,input().split()))
+memo = [0] * n
+memo[1] = abs(lst[0] - lst[1])
+
+for i in range(2,n):
+    memo[i] = min(abs(lst[i] - lst[i-1]) + memo[i-1],abs(lst[i] - lst[i-2]) + memo[i-2])
+print(memo[n-1])
